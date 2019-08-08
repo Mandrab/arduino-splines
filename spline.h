e92cbbf1977e27797c7e8ee44d38fc9b447cc835
Paolo Baldini <paolobaldini01@gmail.com>
2019-08-08 22:17:48 +0200

    removed warnings and error

--- spline.h
+++ spline.h
@@ -6,37 +6,41 @@
 #ifndef spline_h
 #define spline_h
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
 #include "WProgram.h"
+#endif
 
 #define Hermite 10
 #define Catmull 11
 
 class Spline
 {
-  public:
-    Spline( void );
-    Spline( float x[], float y[], int numPoints, int degree = 1 );
-    Spline( float x[], float y[], float m[], int numPoints );
-    float value( float x );
-    void setPoints( float x[], float y[], int numPoints );
-    void setPoints( float x[], float y[], float m[], int numPoints );
-    void setDegree( int degree );
-    
-  private:
-    float calc( float, int);
-    float* _x;
-    float* _y;
-    float* _m;
-    int _degree;
-    int _length;
-    int _prev_point;
-    
-    float hermite( float t, float p0, float p1, float m0, float m1, float x0, float x1 );
-    float hermite_00( float t );
-    float hermite_10( float t );
-    float hermite_01( float t );
-    float hermite_11( float t );
-    float catmull_tangent( int i );
+public:
+	Spline(void);
+	Spline(float x[], float y[], int numPoints, int degree = 1);
+	Spline(float x[], float y[], float m[], int numPoints);
+	float value(float x);
+	void setPoints(float x[], float y[], int numPoints);
+	void setPoints(float x[], float y[], float m[], int numPoints);
+	void setDegree(int degree);
+
+private:
+	float calc(float, int);
+	float *_x;
+	float *_y;
+	float *_m;
+	int _degree;
+	int _length;
+	int _prev_point;
+
+	float hermite(float t, float p0, float p1, float m0, float m1, float x0, float x1);
+	float hermite_00(float t);
+	float hermite_10(float t);
+	float hermite_01(float t);
+	float hermite_11(float t);
+	float catmull_tangent(int i);
 };
 
 #endif
