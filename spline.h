9a29b74b87a8de1b71e4a88f110a074cfb657925
Paolo Baldini <paolobaldini01@gmail.com>
2019-08-10 14:22:36 +0200

    indented

--- spline.h
+++ spline.h
@@ -7,9 +7,9 @@
 #define spline_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-#include "Arduino.h"
+	#include "Arduino.h"
 #else
-#include "WProgram.h"
+	#include "WProgram.h"
 #endif
 
 #define Hermite 10
